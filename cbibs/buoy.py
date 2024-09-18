6844fac149735b4e981ba83038c5629c9492a749
Fletcher B Sims <fletcher.sims@gmail.com>
2024-09-18 15:35:27 -0400

    add some docstrings and exception handling

--- cbibs/buoy.py
+++ cbibs/buoy.py
@@ -5,22 +5,36 @@ DEFAULT_DOMAIN = 'mw.buoybay.noaa.gov'
 
 
 class CbibsApiError(Exception):
-    """Base class for all errors/exceptions"""
+    """Base class for all errors/exceptions."""
+
+
+class InvalidInputError(CbibsApiError):
+    """
+    There is a problem with the input the user provided.
+    - Bad value
+    - Invalid API Key
+    """
+
+
+class UnknownError(CbibsApiError):
+    """There is a problem with CBIBS server."""
 
 
 class Cbibs:
-    def __init__(self, key, protocol='https', domain=DEFAULT_DOMAIN, ver='v1', type='json'):
+    def __init__(self, key, protocol='https', domain=DEFAULT_DOMAIN, ver='v1', data_type='json'):
         """Constructor"""
         self.key = key
         if protocol and protocol not in ('http', 'https'):
             protocol = 'https'
-        self.url = protocol + "://" + domain + '/api/' + ver + '/'
+        self.url = f"{protocol}://{domain}/api/{ver}/{data_type}/"
 
     def __enter__(self):
+        """Enter the method."""
         self.session = requests.Session()
         return self
 
     def __exit__(self, *args):
+        """Exit the method and clean up."""
         self.session.close()
         self.session = None
         return False
--- test/test_settings_protocol.py
+++ test/test_settings_protocol.py
@@ -1,8 +1,10 @@
 from cbibs.buoy import Cbibs
 
-buoy = Cbibs('abcd', 'https')
+buoy = Cbibs('abcd')
+buoy_xml = Cbibs('abcd', data_type='xml')
 
 
 # Check that the base url is being correctly generated
 def test_settings_protocol():
-    assert buoy.url == 'https://mw.buoybay.noaa.gov/api/v1/'
+    assert buoy.url == 'https://mw.buoybay.noaa.gov/api/v1/json/'
+    assert buoy_xml.url == 'https://mw.buoybay.noaa.gov/api/v1/xml/'
