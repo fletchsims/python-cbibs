de51ce1a8a00ffacc36fb14ae477eafb027ba8c1
Fletcher B Sims <fletcher.sims@gmail.com>
2024-12-10 08:16:02 -0500

    update the demo app to show how to use both json and xml

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import os
+import xml.etree.ElementTree as ET
 
 from dotenv import load_dotenv
 
@@ -7,8 +8,9 @@ from cbibs.buoy import Cbibs
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 buoy = Cbibs(api_key=API_KEY)
+buoy_xml = Cbibs(api_key=API_KEY, response_format='xml')
 
-latest_all = buoy.get_current_readings_all_stations()
+latest_all_xml = buoy_xml.get_current_readings_all_stations()
 station_readings = buoy.get_station_readings('AN')
 station_variable = buoy.query_station(
     station_name="AN",
@@ -16,3 +18,10 @@ station_variable = buoy.query_station(
     end_date='2020-04-01T20:00:00z',
     variable="sea_water_temperature"
 )
+
+print('Printing XML Version')
+ET.indent(latest_all_xml)
+print(ET.tostring(latest_all_xml, encoding='unicode'))
+
+print('Printing JSON Version')
+print(station_readings)
